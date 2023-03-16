3ffec672042bdba95644b35288a23ca5d1f81b12
tanmays62343 <tanmays62343@gmail.com>
2023-03-16 11:15:41 +0530

    Done till Q4

---
+++ Q3_Arr.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Q3_Arr {           //Kth smallest element
+    public static void main(String[] args) {
+
+        int [] arr = {10,20,30,40,50,60,70};
+        try {
+            System.out.println(KthSmallest(arr, 3));
+        }
+        catch (Exception e){
+            System.out.println("Invalid input");
+        }
+    }
+
+    public static int KthSmallest(int [] arr,int k){
+        Arrays.sort(arr);
+
+        return arr[k-1];
+    }
+}
