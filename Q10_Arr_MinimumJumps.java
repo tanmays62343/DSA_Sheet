b33164239c66237bb64248c3dc16125273af00b8
tanmays62343 <tanmays62343@gmail.com>
2023-03-23 14:14:52 +0530

    23-Mar-23

--- Q10_Arr_MinimumJumps.java
+++ Q10_Arr_MinimumJumps.java
@@ -1,17 +1,19 @@
 public class Q10_Arr_MinimumJumps {
     public static void main(String[] args) {
         int [] arr = {1,4,3,2,6,7};
-
+        System.out.println(minJumps(arr));
     }
     public static int minJumps(int [] arr){
-        int jumps;
-        int possible;
-        int max = 0 , min = Integer.MAX_VALUE;
-        int n = arr.length;
-
-        for(int i=0; i<n; i++){
-            
-        }
+       int jump=0, start=0, end =0;
+       int n = arr.length;
+       for(int i=0; i<n-1; i++){
+           end = Math.max(end, arr[i]+i);
+           if(start == i){
+               start = end;
+               jump++;
+           }
+       }
+       if(start<n-1) return -1;
+       return jump;
     }
-
 }
